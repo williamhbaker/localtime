d0d5accd0955a678a17b6316bcf08e1b2aa78fb0
Will Baker <wbaker@gmail.com>
2023-06-14 12:39:41 -0400

    try to parse rfc3339nano

--- main.go
+++ main.go
@@ -11,17 +11,21 @@ import (
 )
 
 func main() {
+	now := time.Now()
+
 	r, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatal(fmt.Errorf("reading from stdin: %w", err))
 	}
 
-	parsed, err := dateparse.ParseLocal(string(r))
+	parsed, err := time.Parse(time.RFC3339Nano, string(r))
 	if err != nil {
-		log.Fatal(fmt.Errorf("parsing stdin as datetime: %w", err))
+		parsed, err = dateparse.ParseLocal(string(r))
+		if err != nil {
+			log.Fatal(fmt.Errorf("parsing stdin as datetime: %w", err))
+		}
 	}
 
-	now := time.Now()
 	since := now.Sub(parsed).Truncate(1 * time.Second).String()
 
 	fmt.Printf("%s (%s ago)\n", parsed.Local().Format("Jan _2 2006 03:04:05PM MST"), since)
